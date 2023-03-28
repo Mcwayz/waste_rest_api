b5aa454548c3aafd3a1a2c449d2c386199c80709
Mcwayz <mcwayzj@gmail.com>
2023-03-28 17:21:12 +0200

    second commit

--- api/urls.py
+++ api/urls.py
@@ -2,12 +2,21 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.apiOverview),
     path('subscriptions', views.getSubscription),
-    path('waste', views.getWaste),
+    path('waste-types', views.getWaste),
     path('collections', views.getCollections),
     path('users', views.getUsers),
-    path('addUser', views.addUser),
-    path('addCollection', views.addCollection),
-    path('addWasteType', views.addWaste),
-    path('addSubscription', views.addSubscription),
+    path('add-user', views.addUser),
+    path('add-collection', views.addCollection),
+    path('add-wastetype', views.addWaste),
+    path('add-subscription', views.addSubscription),
+    path('subscription-details/<str:pk>', views.subscriptionDetails, name='subscription-details'),
+    path('waste-details/<str:pk>/', views.wasteDetails, name='waste-details'),
+    path('collection-details/<str:pk>/', views.collectionDetails, name='collection-details'),
+    path('user-details/<str:pk>/', views.getUser, name='user-details'),
+    path('update-user/<str:pk>/', views.updateUser, name='update-user'),
+    path('update-collection/<str:pk>/', views.updateCollection, name='update-collection'),
+    path('update-waste/<str:pk>/', views.updateWaste, name='update-waste'),
+    path('update-subscription/<str:pk>/', views.updateUser, name='update-subscription'),
 ]
--- api/views.py
+++ api/views.py
@@ -2,12 +2,44 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from base.models import Subscription, Collection, Waste, User
 from .serializers import SubSerializer, WasteSerializer, CollectionSerializer, UserSerializer
+from django.shortcuts import get_object_or_404, render
+from django.http import JsonResponse
+
+
+@api_view(['GET'])
+def apiOverview(request):
+    api_urls ={
+        'Users List' : '/users/',
+        'Add User' : '/add-user/',
+        'Waste Types' : '/waste-types/',
+        'Collections' : '/collections/',
+        'Collections' : '/collections/',
+        'Subscriptions' : '/subscriptions/',
+        'Add Waste Type' : '/add-wastetype/',
+        'Add Collection' : '/add-collection/',
+        'Add Subscription' : '/add-subscription/',
+        'Update Waste Type' : '/update-wastetype/',
+        'Update Collection' : '/update-collection/',
+        'Update Subscription' : '/update-subscription/',
+    }
+    return Response(api_urls)
+
+
+
 
 @api_view(['GET'])
 def getSubscription(request):
-    Sub  = Subscription.objects.all()
+    Sub  = Subscription.objects.all() 
     serializer = SubSerializer(Sub, many=True)
     return Response(serializer.data) 
+
+
+
+@api_view(['GET'])
+def subscriptionDetails(request, pk):
+    subscription = get_object_or_404(Subscription, pk=pk)
+    serializer = SubSerializer(subscription)
+    return Response(serializer.data)
 
 
 @api_view(['GET'])
@@ -18,10 +50,24 @@ def getWaste(request):
 
 
 @api_view(['GET'])
+def wasteDetails(request, pk):
+    waste = get_object_or_404(Waste, pk=pk)
+    serializier = WasteSerializer(waste)
+    return Response(serializier.data)
+
+
+@api_view(['GET'])
 def getCollections(request):
     collections = Collection.objects.all()
     serializer = CollectionSerializer(collections, many=True)
     return Response(serializer.data) 
+
+
+@api_view(['GET'])
+def collectionDetails(request, pk):
+    collection = get_object_or_404(Collection, pk=pk)
+    serializer = CollectionSerializer(collection)
+    return Response(serializer.data)
 
 
 @api_view(['GET'])
@@ -31,9 +77,25 @@ def getUsers(request):
     return Response(serializer.data)
 
 
+@api_view(['GET'])
+def getUser(request, pk):
+    users = get_object_or_404(User, pk=pk)
+    serializer = UserSerializer(users)
+    return Response(serializer.data)
+
+
 @api_view(['POST'])
 def addUser(request):
     serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def updateUser(request, pk):
+    user = User.objects.get(user_id=pk)
+    serializer = UserSerializer(instance=user, data=request.data)
     if serializer.is_valid():
         serializer.save()
     return Response(serializer.data)
@@ -48,8 +110,26 @@ def addCollection(request):
 
 
 @api_view(['POST'])
+def updateCollection(request, pk):
+    collection = Collection.objects.get(collection_id=pk)
+    serializer = CollectionSerializer(instance=collection,data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
 def addWaste(request):
     serializer = WasteSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def updateWaste(request, pk):
+    waste = Waste.objects.get(waste_id=pk)
+    serializer = WasteSerializer(instance=waste,data=request.data)
     if serializer.is_valid():
         serializer.save()
     return Response(serializer.data)
@@ -62,3 +142,11 @@ def addSubscription(request):
         serializer.save()
     return Response(serializer.data)
 
+
+@api_view(['POST'])
+def updateSubscription(request, pk):
+    sub = Subscription.objects.get(sub_id=pk)
+    serializer = SubSerializer(instance=sub,data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
