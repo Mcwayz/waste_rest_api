cf8ef9d6b5ed88e4ee9d213f1209d60e3740963d
Niza Tembo <mcwayzj@gmail.com>
2024-02-15 15:25:38 +0200

    mod

--- frontend/urls.py
+++ frontend/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='Index'),
-    path('map', views.map_view, name='map'),
     path('manage-users', views.users, name='Users'),
     path('dashboard', views.dashboard, name='Dash'),
     path('add-user', views.add_user, name='Add User'),
