ac678916ee288c7a5ce68a0610638d2153444f77
Niza Tembo <mcwayzj@gmail.com>
2024-06-10 17:13:20 +0200

    structure

--- frontend/forms.py
+++ frontend/forms.py
@@ -1,11 +1,16 @@
 from django import forms
 from base.models import Waste, ServiceCharge
 
+
+# Waste Type Form
+
+
 class WasteForm(forms.ModelForm):
     class Meta:
         model = Waste
         fields = ['waste_type', 'waste_desc', 'collection_price']
-        
+   
+#  Charge / Commission Form 
         
 class ChargeForm(forms.ModelForm):
     class Meta:
